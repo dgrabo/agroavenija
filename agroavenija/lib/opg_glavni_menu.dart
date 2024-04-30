f4c5cc6b514069e5ec5e3696dbd3d88c33ca621e
David Grabovac <grabovacdado@gmail.com>
2024-04-30 17:30:45 +0200

    supabase commit

--- agroavenija/lib/opg_glavni_menu.dart
+++ agroavenija/lib/opg_glavni_menu.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 import 'header_kupac.dart';
 
 class OpgGlavniMenu extends StatefulWidget {
